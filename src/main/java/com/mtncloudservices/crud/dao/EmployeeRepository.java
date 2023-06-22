49e3e12a533829cff24c2c385c9c742c5bd942ec
mnash29 <mtntrades10@gmail.com>
2023-06-22 16:08:44 -0700

    Defined rest resource, set default page size

--- src/main/java/com/mtncloudservices/crud/dao/EmployeeRepository.java
+++ src/main/java/com/mtncloudservices/crud/dao/EmployeeRepository.java
@@ -2,5 +2,7 @@ package com.mtncloudservices.crud.dao;
 
 import com.mtncloudservices.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="employees") // default adds an 's' to the entity
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
